db03c24df25af1a000e61d2c49b3211f2deffebd
Ramtin Rassoli <ramtinr@ecobee.com>
2019-04-17 23:11:09 -0400

    Update agent base model

--- common/base_agent.py
+++ common/base_agent.py
@@ -5,3 +5,11 @@ class BaseAgent(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def act(self, state, *args, **kwargs):
         pass
+
+    @abc.abstractmethod
+    def save(self, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def load(self, *args, **kwargs):
+        pass
